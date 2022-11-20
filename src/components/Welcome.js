f28dda926d6e1e6fa25775879ac5024402edb6a5
azin-a <aalahkhani@gmail.com>
2022-11-20 21:54:47 +0330

    signup form

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,9 +1,5 @@
-import React from 'react'
-
+import React from "react";
 
 export default function Welcome() {
-  return (
-    <h3> welcome! sign up to enjoy the full ride!</h3>
-  )
+  return <h3> welco welcome! sign up to enjoy the full ride!</h3>;
 }
- 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import auth from './Auth'
+import { reducer as formReducer } from "redux-form";
 
 export default combineReducers({
-    auth
+    auth,
+    form : formReducer,
 })
