b6c9f6e662974dad851ea4a8f473fa0f89625d84
azin-a <aalahkhani@gmail.com>
2022-11-20 18:30:23 +0330

    reducers

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit'
 
 import App from './components/App'
 import Welcome from './components/Welcome';
 import Signup from './components/Auth/signup';
+import reducers from './reducers';
+
 
 const container = document.querySelector('#root')
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
-    <BrowserRouter>
+    <Provider store={configureStore(reducers, {})}>
+        <BrowserRouter>
         <App>
             <Routes>
-            <Route path='/' element={Welcome()}></Route>
+            <Route path='/' element={<Welcome />}></Route>
             <Route path='/signup' element={<Signup />}></Route>
             </Routes>
 
         </App>
     </BrowserRouter>
+    </Provider>
+    
 
 
 );
